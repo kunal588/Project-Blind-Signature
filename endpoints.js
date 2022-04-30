179b5800cac7de5ffef5754fcf4571ab0898f56a
Bharat-mtr <bharatagmtr.999@gmail.com>
2022-04-30 22:59:48 +0530

    final

--- endpoints.js
+++ endpoints.js
@@ -1,28 +1,33 @@
 data = require("./data/keydata.json");
 const express = require("express");
 const router = express.Router();
-const { blindSign, inv } = require("./functions/signFunc");
+const { blindSign, inv, mod_exp } = require("./functions/signFunc");
 
 router.post("/", (req, res) => {
-	try {
-		//apply random operation (now skipping)
-		console.log(typeof req.body.msg);
-		console.log(req.body.msg);
-		const rand = Math.floor(Math.random() * data.n);
-		const signed_msg = (req.body.msg * Math.pow(rand, data.e)) % data.n;
-		// You signed the message as ...
-		let ret_msg = blindSign(signed_msg);
-		ret_msg = (ret_msg * inv(rand)) % data.n;
+  try {
+    //apply random operation (now skipping)
+    console.log(typeof req.body.msg);
+    console.log(req.body.msg);
 
-		// The decrypted message is:
-		const dec_msg = Math.pow(ret_msg, data.e) % data.n;
-		console.log(dec_msg);
-		console.log(ret_msg, signed_msg);
-		// The message after the bank signed
-		res.send({ ret_msg, signed_msg });
-	} catch (err) {
-		console.log(err);
-	}
+    //Genrating Random no. ranging from 1 to n-1
+    const rand = Math.floor(Math.random() * data.n);
+
+    //Multiplying Msg from with Blind factor m' = (m.(r^e))%n
+    const signed_msg = (req.body.msg * mod_exp(rand, data.e, data.n)) % data.n;
+
+    // Sending Encrypted Message to Voting Authority s' = ((m'^d)%n)
+    let ret_msg1 = blindSign(signed_msg);
+
+    // Removing Blind Factor :- s = (s'/r%n)%n
+    let ret_msg = (ret_msg1 * inv(rand)) % data.n;
+
+    // We may also decrypt the message to , it will give us original back :- m =(s^e)%n
+    const dec_msg = mod_exp(ret_msg, data.e, data.n);
+
+    res.send({ signed_msg, ret_msg1, ret_msg });
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 module.exports = router;
--- functions/signFunc.js
+++ functions/signFunc.js
@@ -1,27 +1,51 @@
 data = require("../data/keydata.json");
 const n = data.n,
-	d = data.d;
+  d = data.d;
 
-function inv(num) {
-	return mod_exp(num, data.phin, n);
+function inv(a) {
+  let m = n;
+  [a, m] = [Number(a), Number(m)];
+
+  if (Number.isNaN(a) || Number.isNaN(m)) {
+    return NaN; // invalid input
+  }
+  a = ((a % m) + m) % m;
+  if (!a || m < 2) {
+    return NaN; // invalid input
+  }
+  // find the gcd
+  const s = [];
+  let b = m;
+  while (b) {
+    [a, b] = [b, a % b];
+    s.push({ a, b });
+  }
+  if (a !== 1) {
+    return NaN; // inverse does not exists
+  }
+  // find the inverse
+  let x = 1;
+  let y = 0;
+  for (let i = s.length - 2; i >= 0; --i) {
+    [x, y] = [y, x - y * Math.floor(s[i].a / s[i].b)];
+  }
+  return ((y % m) + m) % m;
 }
 
 function mod_exp(a, b, n) {
-	if (b === 0) return 1;
+  if (b === 0) return 1;
 
-	let c = mod_exp(a, Math.floor(b / 2), n),
-		ans = 1;
+  let c = mod_exp(a, Math.floor(b / 2), n),
+    ans = 1;
 
-	if (b % 2) ans = a;
+  if (b % 2) ans = a;
 
-	return (ans * ((c * c) % n)) % n;
+  return (ans * ((c * c) % n)) % n;
 }
 
 function blindSign(msg) {
-	let m = msg;
-	return mod_exp(m, d, n);
+  let m = msg;
+  return mod_exp(m, d, n);
 }
 
-// console.log(blindSign(56));
-
-module.exports = { blindSign, inv };
+module.exports = { blindSign, inv, mod_exp };
